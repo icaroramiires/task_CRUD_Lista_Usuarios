3b504623534195713a4e6ad4d2698ada4f632dc4
Icaro Ramires <icarostylle@gmail.com>
2016-08-23 18:13:58 -0300

    add toastr mensagem

--- app/controllers/usuario.controller.js
+++ app/controllers/usuario.controller.js
@@ -2,31 +2,33 @@ angular
     .module('app')
     .controller('UsuarioController', UsuarioController);
 
-    function UsuarioController($filter) {
+    function UsuarioController(toastr) {
         let vm = this;
-        id = 0;
         vm.perfis = ["Administrador", "Padrão"];
-
         vm.usuario = {};
+
         vm.usuarios = [{
             id: 0,
-            dataRegistro: "2015-03-24",
+            dataRegistro: new Date("2015-03-24"),
             nome: "Leurimar da Silva Lins",
             email: "leurimar.lins@gmail.com",
             perfil: "Administrador",
             status: "Ativo",
             telefone: "(83) 98827-2291"
         }];
+
         vm.cadastrar = cadastrar;
         vm.save = save;
         vm.alterar = alterar;
         vm.listar = listar;
         vm.deletar = deletar;
 
+        let id = 0;
         function cadastrar(usuario) {
             usuario.id = ++id;
             usuario.status = 'Pendente';
             vm.usuarios.push(angular.copy(usuario));
+            toastr.success('Usuário salvo com sucesso com sucesso', 'Sucesso');
             vm.usuario = {};
         };
 
@@ -42,6 +44,7 @@ angular
             vm.usuarios.filter(function(item){
                 if(usuario.id === item.id) {
                     item = angular.copy(usuario);
+                    toastr.success('Usuário atualizado com sucesso', 'Sucesso');
                 }
             });
         }
@@ -52,5 +55,6 @@ angular
         function deletar(usuario) {
             let index = vm.usuarios.indexOf(usuario);
             vm.usuarios.splice(index, 1);
+             toastr.success('Usuário removido com sucesso', 'Sucesso');
         };
     };
