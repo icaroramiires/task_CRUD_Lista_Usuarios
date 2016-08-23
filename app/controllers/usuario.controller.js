41ce1010f7e56e13c1dfcbda64abf325244bdb00
Icaro Ramires <icarostylle@gmail.com>
2016-08-23 16:21:13 -0300

    implemented crud functions

--- app/controllers/usuario.controller.js
+++ app/controllers/usuario.controller.js
@@ -2,32 +2,54 @@ angular
     .module('app')
     .controller('UsuarioController', UsuarioController);
 
-    function UsuarioController() {
+    function UsuarioController($filter) {
         let vm = this;
+        id = 0;
         vm.perfis = ["Administrador", "Padrão"];
 
+        vm.usuario = {};
         vm.usuarios = [{
-            dataRegistro: "22/08/2016",
+            id: 0,
+            dataRegistro: "2015-03-24",
             nome: "Leurimar da Silva Lins",
             email: "leurimar.lins@gmail.com",
             perfil: "Administrador",
             status: "Ativo",
             telefone: "(83) 98827-2291"
         }];
-
         vm.cadastrar = cadastrar;
+        vm.save = save;
+        vm.alterar = alterar;
         vm.listar = listar;
-        vm.reset = reset;
+        vm.deletar = deletar;
 
         function cadastrar(usuario) {
-            vm.usuarios.push(usuario);
+            usuario.id = ++id;
+            usuario.status = 'Pendente';
+            vm.usuarios.push(angular.copy(usuario));
+            vm.usuario = {};
         };
 
+        function save(usuario) {
+            if(usuario.id == undefined) {
+                cadastrar(usuario);
+            } else {
+                alterar(usuario);
+            }
+        };
+
+        function alterar(usuario) {
+            vm.usuarios.filter(function(item){
+                if(usuario.id === item.id) {
+                    item = angular.copy(usuario);
+                }
+            });
+        }
         function listar() {
             return vm.usuarios;
         };
 
-        function reset() {
-
-        }
+        function deletar(index) {
+            vm.usuarios.splice(index, 1);
+        };
     };
