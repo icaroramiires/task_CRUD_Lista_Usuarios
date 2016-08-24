5dab6664d7be75cb75d453575fd204354f3d554a
Icaro Ramires <icarostylle@gmail.com>
2016-08-24 00:34:18 -0300

    refatoração controller

--- app/controllers/usuario.controller.js
+++ app/controllers/usuario.controller.js
@@ -2,65 +2,66 @@ angular
     .module('app')
     .controller('UsuarioController', UsuarioController);
 
-    function UsuarioController(toastr) {
-        let vm = this;
-        vm.perfis = ["Administrador", "Padrão"];
-        vm.perfilInicial = vm.perfis[1];
+function UsuarioController(toastr) {
+    let vm = this;
+    vm.perfis = ["Administrador", "Padrão"];
+    vm.perfilInicial = vm.perfis[1];
 
-        vm.usuarios = [{
-            id: 0,
-            dataRegistro: new Date("2015-03-24"),
-            nome: "Leurimar da Silva Lins",
-            email: "leurimar.lins@gmail.com",
-            perfil: "Administrador",
-            status: "Ativo",
-            telefone: "(83) 98827-2291"
-        }];
+    vm.usuarios = [{
+        id: 0,
+        dataRegistro: new Date("2015-03-24"),
+        nome: "Leurimar da Silva Lins",
+        email: "leurimar.lins@gmail.com",
+        perfil: "Administrador",
+        status: "Ativo",
+        telefone: "(83) 98827-2291"
+    }];
 
-        vm.cadastrar = cadastrar;
-        vm.save = save;
-        vm.alterar = alterar;
-        vm.listar = listar;
-        vm.deletar = deletar;
-        vm.carregarUsuario = carregarUsuario;
 
-        let id = 0;
-        function cadastrar(usuario) {
-            usuario.id = ++id;
-            usuario.perfil = vm.perfilInicial;
-            usuario.status = 'Pendente';
-            vm.usuarios.push(angular.copy(usuario));
-            toastr.success('Usuário salvo com sucesso com sucesso', 'Sucesso');
-            vm.usuario = {};
-        };
+    let id = 0;
+    vm.cadastrar = cadastrar;
 
-        function save(usuario) {
-            if(usuario.id == undefined) {
-                cadastrar(usuario);
-            } else {
-                alterar(usuario);
-            }
-        };
-
-        function carregarUsuario(usuario) {
-            vm.usuario = angular.copy(usuario);
-        };
-
-        function alterar(usuario) {
-            vm.usuarios.filter(function(item){
-                if(usuario.id === item.id) {
-                    item = angular.copy(usuario);
-                    toastr.success('Usuário atualizado com sucesso', 'Sucesso');
-                }
-            });
-        }
-        function listar() {
-            return vm.usuarios;
-        };
-
-        function deletar(usuario) {
-            let index = vm.usuarios.indexOf(usuario);
-            vm.usuarios.splice(index, 1);
-             toastr.success('Usuário removido com sucesso', 'Sucesso');
-        };
+    function cadastrar(usuario) {
+        usuario.id = ++id;
+        usuario.perfil = vm.perfilInicial;
+        usuario.status = 'Pendente';
+        vm.usuarios.push(angular.copy(usuario));
+        toastr.success('Usuário salvo com sucesso com sucesso', 'Sucesso');
+        vm.usuario = {};
     };
+
+    vm.save = save;
+
+    function save(usuario) {
+        if (usuario.id == undefined) {
+            cadastrar(usuario);
+        } else {
+            alterar(usuario);
+        }
+    };
+
+    vm.alterar = alterar;
+
+    function alterar(usuario) {
+        vm.usuarios.filter(function(item) {
+            if (usuario.id === item.id) {
+                item = angular.copy(usuario);
+                toastr.success('Usuário atualizado com sucesso', 'Sucesso');
+            }
+        });
+    };
+
+    vm.listar = listar;
+
+    function listar() {
+        return vm.usuarios;
+    };
+
+    vm.deletar = deletar;
+
+    function deletar(usuario) {
+        let index = vm.usuarios.indexOf(usuario);
+        vm.usuarios.splice(index, 1);
+        toastr.success('Usuário removido com sucesso', 'Sucesso');
+    };
+};
